3573dc6012fb13a48f59494619e071c6c31592d3
stardust-bytes <thanhdp1305@gmail.com>
2026-06-07 16:11:08 +0700

    feat: integrate FilesView into AppShell

--- frontend/src/components/AppShell.vue
+++ frontend/src/components/AppShell.vue
@@ -2,7 +2,8 @@
   <div class="flex flex-col h-screen bg-cyber-bg font-mono overflow-hidden">
     <div class="flex flex-1 overflow-hidden">
       <SidebarNav :active-view="activeView" @navigate="activeView = $event" />
-      <SettingsView v-if="activeView === 'settings'" class="flex-1 overflow-hidden" />
+      <FilesView v-if="activeView === 'files'" class="flex-1 overflow-hidden" />
+      <SettingsView v-else-if="activeView === 'settings'" class="flex-1 overflow-hidden" />
       <TasksView v-else-if="activeView === 'tasks'" class="flex-1 overflow-hidden" />
       <ChatPanel v-else class="flex-1 overflow-hidden" />
     </div>
@@ -20,6 +21,7 @@ import SidebarNav from './SidebarNav.vue'
 import ChatPanel from './ChatPanel.vue'
 import TasksView from './TasksView.vue'
 import SettingsView from './SettingsView.vue'
+import FilesView from './FilesView.vue'
 import StatusBar from './StatusBar.vue'
 
 const activeView = ref<'chat' | 'tasks' | 'files' | 'settings'>('chat')
