2c4ecb827a1d11b4377bbdd2e926f2f3a67fe569
stardust-bytes <thanhdp1305@gmail.com>
2026-06-07 14:48:16 +0700

    feat: add TasksGateway with Socket.io emit methods TDD

---
+++ backend/src/tasks/tasks.gateway.ts
@@ -0,0 +1,23 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { Task } from '@prisma/client';
+
+@WebSocketGateway({
+  namespace: '/tasks',
+  cors: { origin: ['http://localhost:5173', 'http://localhost:3000'] },
+})
+export class TasksGateway {
+  @WebSocketServer() server: Server;
+
+  emitCreated(task: Task): void {
+    this.server.emit('task:created', task);
+  }
+
+  emitUpdated(task: Task): void {
+    this.server.emit('task:updated', task);
+  }
+
+  emitDeleted(id: number): void {
+    this.server.emit('task:deleted', { id });
+  }
+}
