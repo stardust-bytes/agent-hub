0511d80881937ef9469c43fe5fea3fe716e2a440
stardust-bytes <thanhdp1305@gmail.com>
2026-06-07 14:49:45 +0700

    chore: wire TasksGateway into TasksModule

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TasksGateway } from './tasks.gateway';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksGateway],
 })
 export class TasksModule {}
