c6186aa80b7dd6cd970b2029b9f6fb9b6d331c9c
stardust-bytes <thanhdp1305@gmail.com>
2026-06-13 00:50:22 +0700

    feat: export Gmail, Calendar, Drive services from connector module

--- backend/src/connector/connector.module.ts
+++ backend/src/connector/connector.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConnectorService } from './connector.service';
 import { ConnectorController } from './connector.controller';
 import { GoogleOAuthService } from './providers/google/google-oauth.service';
+import { GmailService } from './providers/google/gmail.service';
+import { GoogleCalendarService } from './providers/google/google-calendar.service';
+import { GoogleDriveService } from './providers/google/google-drive.service';
 
 @Module({
   controllers: [ConnectorController],
-  providers: [ConnectorService, GoogleOAuthService],
-  exports: [ConnectorService, GoogleOAuthService],
+  providers: [ConnectorService, GoogleOAuthService, GmailService, GoogleCalendarService, GoogleDriveService],
+  exports: [ConnectorService, GoogleOAuthService, GmailService, GoogleCalendarService, GoogleDriveService],
 })
 export class ConnectorModule {}
